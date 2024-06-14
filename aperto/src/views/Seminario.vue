2f1f2724e6b78041eec6b0466341dbcad682df0b
PierB <pbasile55@outlook.it>
2024-06-14 13:07:42 +0200

    seminario contenuti

--- aperto/src/views/Seminario.vue
+++ aperto/src/views/Seminario.vue
@@ -1,18 +1,48 @@
 <template>
     <div>
-      <div v-if="seminarDetails">
-        <h2>{{ seminarDetails.title.rendered }}</h2>
+      <section v-if="seminarDetails">
+        <h1>{{ seminarDetails.title.rendered }}</h1>
+        <h2>{{ formattedDate }}</h2>
+        <h2>{{ seminarDetails.seminario_location }}</h2>
+        <section>
+          <p>{{ seminarDetails.descrizione }}</p>
+        </section>
+        
         <div v-for="faculty in seminarDetails.seminario_faculty_member" :key="faculty.id">
-          <h3>{{ faculty.post_title }}</h3>
+
+          <h4>{{ faculty.post_title }}</h4>
           <p>{{ faculty.faculty_member_bio }}</p>
+          <figure>
+            <img :src="faculty.faculty_member_foto" :alt="faculty.post_title" style="width:100%">
+            <figcaption></figcaption>
+          </figure>
         </div>
-      </div>
+
+        <div v-for="partner in seminarDetails.seminario_partner_nome" :key="partner.id">
+          <figure>
+            <img :src="partner.logo_partner" :alt="partner.nome_partner" style="width:100%">
+            <figcaption></figcaption>
+          </figure>
+          <h4>{{ partner.nome_partner }}</h4>
+        </div>
+
+        <div v-for="promotore in seminarDetails.seminario_promotore_nome" :key="promotore.id">
+          <figure>
+            <img :src="promotore.logo_partner" :alt="promotore.nome_partner" style="width:100%">
+            <figcaption></figcaption>
+          </figure>
+          <h4>{{ promotore.nome_partner }}</h4>
+        </div>
+
+
+
+      </section>
       <p v-else>Caricamento...</p>
     </div>
   </template>
   
   <script>
-  import { ref, onMounted, inject } from 'vue';
+  import { ref, onMounted, inject , computed} from 'vue';
   import { useRoute } from 'vue-router';
   
   export default {
@@ -30,13 +60,49 @@
           console.error('Errore nel recuperare i dettagli del seminario:', error);
         }
       };
+
+      const formattedDate = computed(() => {
+        const startDate = seminarDetails.value["seminario_data-inizio"];
+        const endDate = seminarDetails.value["seminario_data-fine"];
+
+        if (startDate && endDate) {
+          return formatSeminarDates(startDate, endDate);
+        }
+        return "";
+      });
+
+      
+      function formatSeminarDates(startDate, endDate) {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+
+        const startDay = start.getDate();
+        const startMonth = monthToString(start.getMonth()); // Converti il numero del mese in stringa completa
+        const endDay = end.getDate();
+        const endMonth = monthToString(end.getMonth()); // Converti il numero del mese in stringa completa
+
+        if (startMonth === endMonth) {
+          return `${startDay} al ${endDay} ${endMonth}`;
+        } else {
+          return `${startDay} ${startMonth} al ${endDay} ${endMonth}`;
+        }
+      }
+
+      function monthToString(monthIndex) {
+        const months = [
+          "gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno",
+          "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"
+        ];
+        return months[monthIndex];
+      }
   
       onMounted(() => {
         fetchSeminarDetails();
       });
   
       return {
-        seminarDetails
+        seminarDetails,
+        formattedDate
       };
     }
   };
