8dc2ea4120fdebf6a777be8a50c83210b7c0fd2d
PierB <pbasile55@outlook.it>
2024-06-17 18:22:17 +0200

    fix scss compilazione

--- aperto/vite.config.js
+++ aperto/vite.config.js
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+console.log('__dirname:', __dirname);
 export default defineConfig({
+  
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      
     },
   },
   css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/global.scss";`
-      }
-    }
+    extract: true,
+    emptyOutDir: true,
+    outDir: 'src',
+    filename: 'style.css', // Specifica un nome personalizzato per il file di output CSS
   }
 });
