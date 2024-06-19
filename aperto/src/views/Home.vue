6127c9047b5f8e01193fcc859122550a5fe0e7ee
PierB <pbasile55@outlook.it>
2024-06-19 11:10:59 +0200

    get testo home

--- aperto/src/views/Home.vue
+++ aperto/src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <div>
+    <div :class="['sticky-image', { sticky: isSticky }]">
+      <img src="../../public/logo_01.svg" alt="Sticky Image">
+    </div>
+    <div class="content" @scroll="handleScroll" ref="content" style="border: 5px solid blue">
+      
+        <p v-if="testoHome" v-html="testoHome.content.rendered"></p>
+
+
+
+    </div>
+
+  </div>
+  <img src="../../public/logo_02.svg" alt="Sticky Image">
+
     <h1>Elenco dei Seminari</h1>
    
       <div v-for="seminario in seminari" :key="seminario.id">
@@ -8,17 +23,50 @@
         </router-link>
       </div>
 
+
+    
   </div>
 </template>
 
 <script>
-import { ref, onMounted, inject } from 'vue';
+import { defineComponent, ref, onMounted, onBeforeUnmount, inject } from 'vue';
 
 export default {
   name: 'Home',
   setup() {
     const seminari = ref([]);
     const axios = inject('axios');
+    const isSticky = ref(true);
+    const content = ref(null);
+    const testoHome = ref(null);
+
+    const handleScroll = () => {
+      const contentEl = content.value;
+      console.log('scroll top', contentEl.scrollTop)
+      console.log('clientHeight', contentEl.clientHeight)
+      console.log('scrollHeight', contentEl.scrollHeight)
+      console.log('somma',contentEl.scrollTop + contentEl.clientHeight )
+      if (contentEl.scrollTop + contentEl.clientHeight >= contentEl.scrollHeight ) {
+        isSticky.value = false;
+      } else {
+        isSticky.value = true;
+      }
+    };
+
+    onMounted(() => {
+      const contentEl = content.value;
+      contentEl.addEventListener('scroll', handleScroll);
+    });
+
+    onBeforeUnmount(() => {
+      const contentEl = content.value;
+      contentEl.removeEventListener('scroll', handleScroll);
+    });
+
+    const loadTestoHome = async () => {
+      const response = await axios.get('/wp/v2/pages/2')
+      testoHome.value = response.data
+    }
 
     const fetchSeminari = async () => {
       try {
@@ -31,15 +79,47 @@ export default {
 
     onMounted(() => {
       fetchSeminari();
+      loadTestoHome()
     });
 
     return {
-      seminari
+      seminari,
+      isSticky,
+      handleScroll,
+      content,
+      testoHome,
+      paragraphs: Array(30).fill('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut lectus euismod, vehicula quam eu, egestas ante.')
     };
   }
 };
 </script>
-
 <style scoped>
-/* Aggiungi qui i tuoi stili */
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.sticky-image {
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+  background-color: white;
+  padding: 10px;
+  z-index: 1000;
+  transition: all 0.3s ease-in-out;
+}
+
+.sticky-image.sticky {
+  position: sticky;
+}
+
+.content {
+  overflow-y: scroll;
+  padding-top: 20px;
+}
+
+.content p {
+  margin: 20px 0;
+}
 </style>
