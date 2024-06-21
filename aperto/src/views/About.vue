18e452d60b3649a6bd2d2cab0374ac4309e7445d
PierB <pbasile55@outlook.it>
2024-06-21 13:05:27 +0200

    pagina about

--- aperto/src/views/About.vue
+++ aperto/src/views/About.vue
@@ -1,13 +1,37 @@
 <template>
     <div>
-      <h1>About Page</h1>
-      <p>Learn more about us on this page.</p>
+     
+      <p v-if="testoAbout" v-html="testoAbout.content.rendered"></p>
     </div>
   </template>
   
   <script>
+  import { defineComponent, ref, onMounted, onBeforeUnmount, inject } from 'vue';
+
+
   export default {
-    name: 'About'
-  };
+    name: 'About',
+  setup() {
+  const testoAbout = ref(null);
+  const axios = inject('axios');
+
+  const fetchAboutText = async () => {
+      try {
+        const response = await axios.get('/wp/v2/pages/103');
+        testoAbout.value = response.data;
+      } catch (error) {
+        console.error('Errore nel recuperare i seminari:', error);
+      }
+    };
+
+    onMounted(() => {
+      fetchAboutText();
+    });
+    return {
+    testoAbout
+  }
+  }
+ 
+}
   </script>
-  
+ 
