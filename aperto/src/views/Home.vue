8535b23a7bb0b8d5e1df7f49a29e544601583903
PierB <pbasile55@outlook.it>
2024-06-26 16:25:57 +0200

    diffrente nomenclatura per seminario

--- aperto/src/views/Home.vue
+++ aperto/src/views/Home.vue
@@ -68,7 +68,7 @@
 
         <p class="homeSeminari-lista--numero">Seminario {{ seminario.seminari_numero }}</p>
 
-        <p  class="homeSeminari-lista--stato">{{ seminario.stato[0].name }}</p>
+        <p  class="homeSeminari-lista--stato">{{ seminario.stato[0].name == 'Archiviato' ? 'Closed' : 'Open Call' }}</p>
 
         <button v-if="seminario.stato[0].name == 'Corrente' ">Freccia</button>
 
@@ -77,12 +77,12 @@
 
       </router-link>
 
-      <!--Archiviato e Futturo-->
+      <!-- Futuro-->
 
       <a v-else>
         <p class="homeSeminari-lista--numero">Seminario {{ seminario.seminari_numero }}</p>
 
-        <p  class="homeSeminari-lista--stato">{{ seminario.stato[0].name }}</p>
+        <p  class="homeSeminari-lista--stato">Coming soon</p>
 
         <h2>{{ seminario.title.rendered }}</h2>
         <h3>{{ seminario.seminario_data_inizio }}</h3>
