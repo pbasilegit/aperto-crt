89759c38ae24fc714d7f5f1f5b59e42bbaa48731
PierB <pbasile55@outlook.it>
2024-07-09 12:45:58 +0200

    implementazione menu a dropdown

--- aperto/src/components/Header.vue
+++ aperto/src/components/Header.vue
@@ -3,7 +3,18 @@
         <router-link to="/" >
           <img src="https://www.aperto-crt.it/core/wp-content/uploads/2024/07/header_aperto.svg" alt="Sticky Image" :class="{ 'scale-100': isScale100, 'scale-80':!isScale100 }">
         </router-link> 
-        <router-link to="/about" class="nav-links--about" exact-active-class="active-link">About</router-link>  
+        <div @click="toggleMenu" class="menu-button">
+          Menu
+        </div>
+        <div class="dropdown-content" v-show="showMenu">
+          <ul>
+            <li><router-link to="/about">About</router-link></li>
+            <li><router-link to="/seminari">Seminari</router-link></li>
+            <li><router-link to="/partecipanti">Partecipanti</router-link></li>
+            <li><router-link to="/faculty">Faculty</router-link></li>
+            <li><router-link to="/faq">FAQ</router-link></li>
+          </ul>
+        </div>      
       </nav>
 </template>
   
@@ -11,8 +22,12 @@
 import { ref, onUnmounted, onMounted } from 'vue';
 
 const lastScrollTop = ref(0);
+const showMenu = ref(false);
 const isScale100 = ref(true); // Scala 100% quando lo scroll è verso l'alto
 
+function toggleMenu() {
+  showMenu.value = !showMenu.value;
+}
 
 onMounted(() => {
   window.addEventListener('scroll', handleScroll);
@@ -39,3 +54,36 @@ function handleScroll() {
 
 
 
+<style scoped>
+.menu-button {
+  cursor: pointer;
+  /* Stili personalizzati per il pulsante del menu */
+}
+
+.dropdown-content {
+  position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    height: fit-content;
+    background-color: #f9f9f9;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+    text-align: right; 
+}
+
+.dropdown-content ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.dropdown-content li {
+  padding: 12px 16px;
+  cursor: pointer;
+}
+
+.dropdown-content li:hover {
+  background-color: #f1f1f1;
+}
+</style>
