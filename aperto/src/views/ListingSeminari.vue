341a87d2a7249246ec817e9781d9c224cee5ff30
PierB <pbasile55@outlook.it>
2024-07-16 17:43:30 +0200

    struttura listing seminario

--- aperto/src/views/ListingSeminari.vue
+++ aperto/src/views/ListingSeminari.vue
@@ -9,22 +9,33 @@
         <h4>Archivio</h4>
       </button>
     </div>
+
+    <!-- CORRENTI E FUTURI-->
     <section v-if="visibleList === 'list1'" class="tab-content">
       <ul>
-        <li v-for="seminario in seminariCorrenti" :key="seminario.id" class="seminario">
+        <li v-for="seminario in seminariCorrenti" :key="seminario.id" class="seminario" @click="goToSeminario(seminario.id, seminario.stato[0].name)">
+          <p>Seminario {{ seminario.seminari_numero }}</p>
+          <p>{{ seminario.seminario_location }}</p>
           <h3>{{ seminario.title.rendered }}</h3>
-          <p>{{ new Date(seminario.date).toLocaleDateString() }}</p>
-          <span class="job-title">{{ seminario.job_title }}</span>
+          <p>{{ formatSeminarioDate(seminario.seminario_data_inizio, seminario.seminario_data_fine) }}</p>
+          <img v-if="seminario.stato[0].name == 'Corrente'"  src="https://www.aperto-crt.it/core/wp-content/uploads/2024/07/arrow_left.svg" alt="scopri di più" />
+
+          <p class="homeSeminari-lista--stato" v-else >coming soon </p>
+
         </li>
       </ul>
     </section>
+
+    <!-- ARCHIVIATI-->
     <section v-if="visibleList === 'list2'" class="tab-content">
       <ul>
-        <li v-for="seminario in seminariArchiviati" :key="seminario.id" class="seminario">
-          <h3>{{ seminario.title.rendered }}</h3>
-          <p>{{ new Date(seminario.date).toLocaleDateString() }}</p>
-          <span class="job-title">{{ seminario.job_title }}</span>
+        <li v-for="seminario in seminariArchiviati" :key="seminario.id" class="seminario" @click="goToSeminario(seminario.id)">
           <img :src="seminario.immagine_header.guid"/>
+          <p>{{ seminario.seminario_location }}</p>
+
+          <h3>{{ seminario.title.rendered }}</h3>
+          <p>{{ formatSeminarioDate(seminario.seminario_data_inizio, seminario.seminario_data_fine) }}</p>
+          <img v-if="seminario.stato[0].name == 'Archiviato'" src="https://www.aperto-crt.it/core/wp-content/uploads/2024/07/arrow_left.svg" alt="scopri di più" />
 
         </li>
       </ul>
@@ -35,9 +46,10 @@
 <script setup>
 import { computed, onMounted, ref } from 'vue';
 import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
 const emit = defineEmits(['componentReady'])
 const visibleList = ref('list1')
-
+const route = useRouter();
 
 const store = useStore();
 
@@ -46,12 +58,37 @@ const showList = (list) => {
 }
 // Chiamata all'azione Vuex per ottenere i seminari
 onMounted(() => {
+  window.scrollTo(0, 0);
   setTimeout(() => {
     emit('componentReady', true)
   }, 2000)
   store.dispatch('fetchSeminari');
 });
 
+const goToSeminario = (id, stato) =>{
+  if(stato != 'Futuro'){
+    route.push('/seminario/' + id)
+  }
+
+}
+
+const formatSeminarioDate = (dataInizio, dataFine) => {
+      const inizio = new Date(dataInizio);
+      const fine = new Date(dataFine);
+      const annoInizio = inizio.getFullYear();
+
+
+      const giornoInizio = inizio.getDate();
+      const meseInizio = inizio.toLocaleString('it-IT', { month: 'long' });
+      const giornoFine = fine.getDate();
+      const meseFine = fine.toLocaleString('it-IT', { month: 'long' });
+
+      if (meseInizio === meseFine) {
+        return `dal ${giornoInizio} al ${giornoFine} ${meseInizio} ${annoInizio}`;
+      } else {
+        return `dal ${giornoInizio} ${meseInizio} al ${giornoFine} ${meseFine} ${annoInizio}`;
+      }
+    }
 // Getter per ottenere i seminari correnti e archiviati dal Vuex store
 const seminariCorrenti = computed(() => store.getters.seminariCorrenti);
 const seminariArchiviati = computed(() => store.getters.seminariArchiviati);
@@ -67,14 +104,10 @@ li {
   margin-bottom: 1rem;
 }
 
-.seminario {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+img{
+  max-width: 200px;
 }
 
-.job-title {
-  font-weight: bold;
-  color: #666;
-}
+
+
 </style>
